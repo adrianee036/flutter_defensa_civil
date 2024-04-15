fbaa908cc511953afafba0336c36c7a43459c755
Adrian Elias Ureña Gomez <123125025+adrianee036@users.noreply.github.com>
2024-04-15 17:51:58 -0400

    Update nav_bar.dart

--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -149,7 +149,7 @@ class _NavBarState extends State<NavBar> {
               Icons.emergency,
               'Medidas Preventiva',
               () => Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => MedidasPreventivasView())),
+                  context, MaterialPageRoute(builder: (context) => MedidasPreventivasList())),
               true),
           _buildListTile(
               Icons.person_add_rounded,
