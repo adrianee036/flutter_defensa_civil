21d9780097a978ca0dcccf12534cb7f1fd1782c8
alejandrocamilo <114262896+alejandrocamilo@users.noreply.github.com>
2024-04-15 07:57:52 -0400

    Update miembro.dart

--- lib/Main Features/miembro.dart
+++ lib/Main Features/miembro.dart
@@ -1,22 +1,51 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class Miembro extends StatefulWidget {
-  const Miembro({super.key});
-
+class MiembrosView extends StatefulWidget {
   @override
-  State<Miembro> createState() => _MiembroState();
+  _MiembrosViewState createState() => _MiembrosViewState();
 }
 
+class _MiembrosViewState extends State<MiembrosView> {
+  List<Map<String, dynamic>> miembros = [];
 
-class _MiembroState extends State<Miembro> {
+  Future<void> fetchMiembros() async {
+    final response = await http.get(Uri.parse('https://adamix.net/defensa_civil/def/miembros.php'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        miembros = List<Map<String, dynamic>>.from(json.decode(response.body));
+      });
+    } else {
+      throw Exception('Failed to load miembros');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMiembros();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Miembro'),
+        title: Text('Miembros de la Defensa Civil Dominicana'),
       ),
       body: Center(
-        child: Text('Contenido de MiWidget'),
+        child: miembros.isEmpty
+            ? CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: miembros.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(miembros[index]['nombre']),
+                    subtitle: Text(miembros[index]['cargo']),
+                  );
+                },
+              ),
       ),
     );
   }
