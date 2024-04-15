0b372ff99386712b8e276b51ca105d92865beaac
alejandrocamilo <114262896+alejandrocamilo@users.noreply.github.com>
2024-04-15 08:03:20 -0400

    Update noticia_especifica.dart

--- lib/Post Login/noticia_especifica.dart
+++ lib/Post Login/noticia_especifica.dart
@@ -1,22 +1,53 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class NoticiaSpecifica extends StatefulWidget {
-  const NoticiaSpecifica({super.key});
-
+class NoticiasEspecificasView extends StatefulWidget {
   @override
-  State<NoticiaSpecifica> createState() => _NoticiaSpecificaState();
+  _NoticiasEspecificasViewState createState() => _NoticiasEspecificasViewState();
 }
 
+class _NoticiasEspecificasViewState extends State<NoticiasEspecificasView> {
+  List<Map<String, dynamic>> noticias = [];
 
-class _NoticiaSpecificaState extends State<NoticiaSpecifica> {
+  Future<void> fetchNoticias() async {
+    final response = await http.get(Uri.parse('https://adamix.net/defensa_civil/def/noticias_especificas.php'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        noticias = List<Map<String, dynamic>>.from(json.decode(response.body));
+      });
+    } else {
+      throw Exception('Failed to load noticias');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNoticias();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('NoticiaSpecifica'),
+        title: Text('Noticias y Líneas de Acción Específicas'),
       ),
       body: Center(
-        child: Text('Contenido de MiWidget'),
+        child: noticias.isEmpty
+            ? CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: noticias.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(noticias[index]['titulo']),
+                      subtitle: Text(noticias[index]['descripcion']),
+                    ),
+                  );
+                },
+              ),
       ),
     );
   }
